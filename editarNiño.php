c8ba66e12f350e59301606d23efd6fffcb732ec8
DayanaV99 <135269936+DayanaV99@users.noreply.github.com>
2023-12-19 00:07:56 -0500

    EditarNiñoArreglado

--- editarNiño.php
+++ editarNiño.php
@@ -13,19 +13,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submit"])) {
         // Obtener la conexión
         $conexion = Cconexion::ConexionBD();
 
-        // Preparar la llamada al procedimiento almacenado
-        $sql = "EXEC EditarNiño @id_niño=?, @nombre=?, @apellido=?, @edad=?, @telefonopadres=?";
-        $stmt = $conexion->prepare($sql);
-        $stmt->bindParam(1, $id_niño, PDO::PARAM_INT);
-        $stmt->bindParam(2, $nombre, PDO::PARAM_STR);
-        $stmt->bindParam(3, $apellido, PDO::PARAM_STR);
-        $stmt->bindParam(4, $edad, PDO::PARAM_INT);
-        $stmt->bindParam(5, $telefono, PDO::PARAM_STR);
+        // Verificar si el niño existe antes de actualizar
+        $verificar_sql = "SELECT 1 FROM Niño WHERE id_niño = ?";
+        $verificar_stmt = $conexion->prepare($verificar_sql);
+        $verificar_stmt->bindParam(1, $id_niño, PDO::PARAM_INT);
+        $verificar_stmt->execute();
 
-        // Ejecutar la consulta
-        $stmt->execute();
+        if ($verificar_stmt->fetchColumn()) {
+            // Preparar la llamada al procedimiento almacenado
+            $sql = "EXEC EditarNiño @id_niño=?, @nombre=?, @apellido=?, @edad=?, @telefonopadres=?";
+            $stmt = $conexion->prepare($sql);
+            $stmt->bindParam(1, $id_niño, PDO::PARAM_INT);
+            $stmt->bindParam(2, $nombre, PDO::PARAM_STR);
+            $stmt->bindParam(3, $apellido, PDO::PARAM_STR);
+            $stmt->bindParam(4, $edad, PDO::PARAM_INT);
+            $stmt->bindParam(5, $telefono, PDO::PARAM_STR);
 
-        echo "Registro de Niño actualizado correctamente.";
+            // Ejecutar la consulta
+            $stmt->execute();
+
+            // Redirigir después de la edición
+            header("Location: matriculas.php");
+            exit();
+        } else {
+            echo "El niño con ID $id_niño no existe.";
+        }
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     } finally {
@@ -60,18 +72,21 @@ if (isset($_GET["id"])) {
 
 <head>
     <meta charset="utf-8">
-    <title>Editar</title>
+    <title>Editar </title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
+</head>
 
+<body>
     <!-- Favicon -->
     <link href="img/favicon.ico" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap"
+    <link
+        href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap"
         rel="stylesheet">
 
     <!-- Icon Font Stylesheet -->
@@ -87,49 +102,129 @@ if (isset($_GET["id"])) {
 
     <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
-</head>
+    </head>
 
-<body>
+    <body>
 
-    <!-- Navbar Start -->
-    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <!-- ... (código del Navbar) ... -->
-    </nav>
-    <!-- Navbar End -->
 
-    <!-- Header Start -->
-    <div class="container-fluid bg-primary py-5 mb-5 page-header">
-        <!-- ... (código del Header) ... -->
+        <!-- Spinner Start -->
+        <!-- <div id="spinner"
+    class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+        <span class="sr-only">Loading...</span>
     </div>
-    <!-- Header End -->
+</div> -->
+        <!-- Spinner End -->
 
-    <!-- Formulario de Edición de Niño -->
-    <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container">
-            <!-- ... (código del Formulario) ... -->
+
+        <!-- Navbar Start -->
+        <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+            <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+                <h2 class="m-0 text-primary"><i class="fa fa-book me-3"></i>Iglesia Central</h2>
+            </a>
+            <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse"
+                data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ms-auto p-4 p-lg-0">
+                    <a href="parroquias.php" class="nav-item nav-link active">Parroquias</a>
+                    <a href="cursos.php" class="nav-item nav-link">Sacramentos</a>
+                    <a href="guias.php" class="nav-item nav-link">Guias Espirituales</a>
+                </div>
+                <a href="catequista.php" class="btn btn-secondary py-4 px-lg-5 d-none d-lg-block">CATEQUISTA</a>
+
+                <a href="matriculas.php" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">MATRÍCULAS<i
+                        class="fa fa-arrow-right ms-3"></i></a>
+
+            </div>
+        </nav>
+        <!-- Navbar End -->
+
+        <!-- Header Start -->
+        <div class="container-fluid bg-primary py-5 mb-5 page-header">
+            <div class="container py-5">
+                <div class="row justify-content-center">
+                    <div class="col-lg-10 text-center">
+                        <h1 class="display-3 text-white animated slideInDown">Edición de Matrícula</h1>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
+        <!-- Header End -->
 
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-        <!-- ... (código del Footer) ... -->
-    </div>
-    <!-- Footer End -->
 
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+        <!-- matriculas Start -->
+        <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="container">
+                <div class="text-center">
+                    <h6 class="section-title bg-white text-center text-primary px-3">edición</h6>
+                    <h1 class="mb-5">Niños Catecismo</h1>
+                </div>
 
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+                <!-- Formulario Matricula -->
+                <form id="editar-form" action="editarNiño.php" method="POST" class="col-md-6 mx-auto">
+                    <!-- Campo oculto para almacenar el ID del niño -->
+                    <input type="hidden" name="id_niño" value="<?php echo $id_niño; ?>">
 
-    <!-- Template Javascript -->
-    <script src="js/main.js"></script>
-</body>
+                    <!-- Campos del formulario -->
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="nombre" class="form-label">Nombre:</label>
+                            <input type="text" id="nombre" name="nombre" class="form-control"
+                                value="<?php echo $fila['nombre']; ?>" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="apellido" class="form-label">Apellido:</label>
+                            <input type="text" id="apellido" name="apellido" class="form-control"
+                                value="<?php echo $fila['apellido']; ?>" required>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="edad" class="form-label">Edad:</label>
+                            <input type="number" id="edad" name="edad" class="form-control"
+                                value="<?php echo $fila['edad']; ?>" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="telefono" class="form-label">Teléfono Familiar:</label>
+                            <input type="text" id="telefono" name="telefono" class="form-control"
+                                value="<?php echo $fila['telefonopadres']; ?>" required>
+                        </div>
+                    </div>
+
+                    <!-- Botón para enviar el formulario -->
+                    <div class="mb-3 text-center">
+                        <button type="submit" name="submit" class="btn btn-info text-white m-2">Actualizar</button>
+                    </div>
+                </form>
+
+
+                <!-- matriculas End -->
+                <!-- Footer Start -->
+                <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+                    <div class="container py-5">
+                    </div>
+                </div>
+                <!-- Footer End -->
+
+
+                <!-- Back to Top -->
+                <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+                <!-- JavaScript Libraries -->
+                <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+                <script src="lib/wow/wow.min.js"></script>
+                <script src="lib/easing/easing.min.js"></script>
+                <script src="lib/waypoints/waypoints.min.js"></script>
+                <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+                <!-- Template Javascript -->
+                <script src="js/main.js"></script>
+    </body>
 
 </html>
 
--- eliminarCatequista.php
+++ eliminarCatequista.php
@@ -10,6 +10,18 @@ function eliminarCatequista($idCatequista)
     if ($conexion) {
         try {
             // Preparar la llamada al procedimiento almacenado
+            $sql = "EXEC EliminarCatequ<?php
+include_once "conexion.php";
+
+function eliminarCatequista($idCatequista)
+{
+    // Obtener la conexión utilizando el método estático de la clase
+    $conexion = Cconexion::ConexionBD();
+
+    // Verificar si la conexión es válida antes de realizar la consulta
+    if ($conexion) {
+        try {
+            // Preparar la llamada al procedimiento almacenado
             $sql = "EXEC EliminarCatequista @IdCatequista=?";
             $stmt = $conexion->prepare($sql);
             $stmt->bindParam(1, $idCatequista, PDO::PARAM_INT);
@@ -21,24 +33,59 @@ function eliminarCatequista($idCatequista)
             $rowCount = $stmt->rowCount();
 
             if ($rowCount > 0) {
-                header("Location: catequista.php"); // Redirige a la página principal de catequistas
+                echo "Registro de Catequista eliminado correctamente.";
+                ob_end_flush(); // Desactivar el almacenamiento en búfer
+                header("Location: catequista.php"); // Redirige a la página de catequistas
+                exit();
+            } else {
+                echo "No se encontró el registro de Catequista para eliminar.";
+            }
+        } catch (PDOException $e) {
+            echo "Error al ejecutar la consulta: " . $e->getMessage();
+        }
+    } else {
+        echo "Error al establecer la conexión a la base de datos.";
+    }
+}
+
+// Uso de la función de eliminación de Catequista
+if (isset($_GET['id_catequista']) && is_numeric($_GET['id_catequista'])) {
+    $idCatequista = $_GET['id_catequista'];
+    eliminarCatequista($idCatequista);
+} else {
+    echo "ID de Catequista no válido.";
+}
+?>
+ista @IdCatequista=?";
+            $stmt = $conexion->prepare($sql);
+            $stmt->bindParam(1, $idCatequista, PDO::PARAM_INT);
+
+            // Ejecutar el procedimiento almacenado
+            $stmt->execute();
+
+            // Verificar si se afectaron filas (si se eliminó algún registro)
+            $rowCount = $stmt->rowCount();
+
+            if ($rowCount > 0) {
+                ob_end_flush(); // Desactivar el almacenamiento en búfer
+                header("Location: catequista.php"); // Redirige a la página de catequistas
                 exit();
             } else {
                 echo "<script>
                         alert('No se encontró el registro de Catequista para eliminar.');
-                        window.location.href = 'catequista.php'; 
+                        window.location.href = 'catequista.php'; // Redirige a la misma página
                       </script>";
             }
         } catch (PDOException $e) {
             echo "<script>
                     alert('Error al ejecutar la consulta: " . $e->getMessage() . "');
-                    window.location.href = 'catequista.php'; 
+                    window.location.href = 'catequista.php'; // Redirige a la misma página
                   </script>";
         }
     } else {
         echo "<script>
                 alert('Error al establecer la conexión a la base de datos.');
-                window.location.href = 'catequista.php'; 
+                window.location.href = 'catequista.php'; // Redirige a la misma página
               </script>";
     }
 }
