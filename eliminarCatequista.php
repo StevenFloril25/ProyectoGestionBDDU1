945009395bec5c8b5ef7ec458d19205d24f3f0f3
DayanaV99 <135269936+DayanaV99@users.noreply.github.com>
2023-12-19 12:47:28 -0500

    catequista

--- eliminarCatequista.php
+++ eliminarCatequista.php
@@ -3,96 +3,51 @@ include_once "conexion.php";
 
 function eliminarCatequista($idCatequista)
 {
-    // Obtener la conexión utilizando el método estático de la clase
-    $conexion = Cconexion::ConexionBD();
+    try {
+        $conexion = Cconexion::ConexionBD();
 
-    // Verificar si la conexión es válida antes de realizar la consulta
-    if ($conexion) {
-        try {
-            // Preparar la llamada al procedimiento almacenado
-            $sql = "EXEC EliminarCatequ<?php
-include_once "conexion.php";
-
-function eliminarCatequista($idCatequista)
-{
-    // Obtener la conexión utilizando el método estático de la clase
-    $conexion = Cconexion::ConexionBD();
-
-    // Verificar si la conexión es válida antes de realizar la consulta
-    if ($conexion) {
-        try {
-            // Preparar la llamada al procedimiento almacenado
-            $sql = "EXEC EliminarCatequista @IdCatequista=?";
-            $stmt = $conexion->prepare($sql);
-            $stmt->bindParam(1, $idCatequista, PDO::PARAM_INT);
-
-            // Ejecutar el procedimiento almacenado
-            $stmt->execute();
-
-            // Verificar si se afectaron filas (si se eliminó algún registro)
-            $rowCount = $stmt->rowCount();
-
-            if ($rowCount > 0) {
-                echo "Registro de Catequista eliminado correctamente.";
-                ob_end_flush(); // Desactivar el almacenamiento en búfer
-                header("Location: catequista.php"); // Redirige a la página de catequistas
-                exit();
-            } else {
-                echo "No se encontró el registro de Catequista para eliminar.";
-            }
-        } catch (PDOException $e) {
-            echo "Error al ejecutar la consulta: " . $e->getMessage();
+        if (!$conexion) {
+            throw new Exception("Error al establecer la conexión a la base de datos.");
         }
-    } else {
-        echo "Error al establecer la conexión a la base de datos.";
-    }
-}
 
-// Uso de la función de eliminación de Catequista
-if (isset($_GET['id_catequista']) && is_numeric($_GET['id_catequista'])) {
-    $idCatequista = $_GET['id_catequista'];
-    eliminarCatequista($idCatequista);
-} else {
-    echo "ID de Catequista no válido.";
-}
-?>
-ista @IdCatequista=?";
-            $stmt = $conexion->prepare($sql);
-            $stmt->bindParam(1, $idCatequista, PDO::PARAM_INT);
+        // Mensaje de depuración
+        echo "<script>alert('Eliminando catequista con ID: " . $idCatequista . "');</script>";
 
-            // Ejecutar el procedimiento almacenado
-            $stmt->execute();
+        $sql = "EXEC EliminarCatequista @IdCatequista=?";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(1, $idCatequista, PDO::PARAM_INT);
+        $stmt->execute();
 
-            // Verificar si se afectaron filas (si se eliminó algún registro)
-            $rowCount = $stmt->rowCount();
+        $rowCount = $stmt->rowCount();
 
-            if ($rowCount > 0) {
-                ob_end_flush(); // Desactivar el almacenamiento en búfer
-                header("Location: catequista.php"); // Redirige a la página de catequistas
-                exit();
-            } else {
-                echo "<script>
-                        alert('No se encontró el registro de Catequista para eliminar.');
-                        window.location.href = 'catequista.php'; // Redirige a la misma página
-                      </script>";
-            }
-        } catch (PDOException $e) {
+        if ($rowCount > 0) {
             echo "<script>
-                    alert('Error al ejecutar la consulta: " . $e->getMessage() . "');
-                    window.location.href = 'catequista.php'; // Redirige a la misma página
+                    alert('Registro de Catequista eliminado correctamente.');
+                    window.location.href = 'catequista.php'; // Redirige a la página de catequistas
+                  </script>";
+        } else {
+            echo "<script>
+                    alert('No se encontró el registro de Catequista para eliminar.');
+                    window.location.href = 'catequista.php'; // Redirige a la página de catequistas
                   </script>";
         }
-    } else {
+
+    } catch (PDOException $e) {
         echo "<script>
-                alert('Error al establecer la conexión a la base de datos.');
-                window.location.href = 'catequista.php'; // Redirige a la misma página
+                alert('Error al ejecutar la consulta: " . $e->getMessage() . "');
+                window.location.href = 'catequista.php';
+              </script>";
+    } catch (Exception $e) {
+        echo "<script>
+                alert('Error: " . $e->getMessage() . "');
+                window.location.href = 'catequista.php';
               </script>";
     }
 }
 
-// Uso de la función de eliminación de Catequista
 if (isset($_GET['id_catequista']) && is_numeric($_GET['id_catequista'])) {
     $idCatequista = $_GET['id_catequista'];
     eliminarCatequista($idCatequista);
 }
 ?>
+
